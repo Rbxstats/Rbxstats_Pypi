0a6041482b97d428b356dc7cbd43a722fcd70d65
Jermy <penajermy24@gmail.com>
2025-02-22 14:31:00 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="rbxstats",
-    version="0.2.6",
+    version="0.2.7",
     description="A Python client for accessing the RBXStats API.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    author="JermyOIS",
+    author="Rbxstats",
     author_email="rbxstatsxyz@gmail.com",
-    url="https://github.com/Jermy-tech/rbxstats_py",  # Replace with your repo URL
+    url="https://github.com/Rbxstats/Rbxstats_Pypi/tree/main",  # Replace with your repo URL
     packages=find_packages(),
     install_requires=[
         "requests>=2.20.0"
