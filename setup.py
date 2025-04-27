0ebd1a9ab835adc6f5a65830be5c25de7566f2d0
Jermy <penajermy24@gmail.com>
2025-04-26 21:14:41 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="rbxstats",
-    version="3.0.2",
+    version="3.0.3",
     description="A comprehensive client for the RbxStats API",
     author="Rbxstats",
     author_email="rbxstatsxyz@gmail.com",
